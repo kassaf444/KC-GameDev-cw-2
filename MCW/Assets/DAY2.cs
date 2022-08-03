31ff0bd83c5287524b6a30bb99a8969651cae401
kassaf444 <109866307+kassaf444@users.noreply.github.com>
2022-08-03 20:27:52 +0300

    push

---
+++ MCW/Assets/DAY2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+"'name and age "
+public class DAY2 : MonoBehaviour 
+{ int age= 18 
+    // Start is called before the first frame update
+    void Start()
+    {string name = ''khalid'' 
+        print(my  age  is "" + age):
+    }bool is champion = true; 
+   
+"'speed"
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 33.5f; 
+    }print(" the friend name is " + name +"and his age is " +age);
+    print(" the prophecy says that he is a " + is hero + "hero with aspeed of " + speed);
+     "done"
